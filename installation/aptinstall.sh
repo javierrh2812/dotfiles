e053bfb286c022f9485a6d152c49b3bd295cbb6b
Javier Ramos <54128454+javierrh2812@users.noreply.github.com>
2022-11-11 10:49:16 -0500

    Update aptinstall.sh

--- installation/aptinstall.sh
+++ installation/aptinstall.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 printf "\n\n---Installing basics, no tan basics and other stuff\n\n"
-sudo apt update
 function install {
   which $1 &> /dev/null
 
@@ -14,29 +13,29 @@ function install {
 }
 
 # Basics
+install git #probablemente ya tengas git
 install curl
-install git
 install tmux
 
 #zsh
 printf "\n\n---Installing zsh / oh-my-zsh / zshplugins---\n\n"
-install zsh
+#install zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sudo git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
+sudo git clone https://github.com/lukechilds/zsh-better-npm-completion ~/.oh-my-zsh/custom/plugins/zsh-better-npm-completion
 
 #i3
-install i3
-install maim
+#install i3
+#install maim
 #steal from https://github.com/addy-dclxvi/i3-starterpack
 #sudo apt-get install i3-wm dunst i3lock i3status suckless-tools 
 #sudo apt-get install compton hsetroot rxvt-unicode xsel rofi fonts-noto fonts-mplus xsettingsd lxappearance scrot viewnior
 
 #utilities
-install code
 install xclip
 install jq
-install zoxide
 install neofetch
 
 # Fun stuff
